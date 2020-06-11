a90b6b24fe8ad2ee411eca14df7d90674488a617
dazhwu <63244649+dazhwu@users.noreply.github.com>
2020-06-11 14:50:38 -0400

    Update Rpkg_install.R

--- Rpkg_install.R
+++ Rpkg_install.R
@@ -2,4 +2,4 @@
 
 pkgs <- read.csv('rpkgs.csv', header=TRUE);
 pkgvec <- as.vector(pkgs$Package);
-install.packages(c(pkgvec), quiet = TRUE, keep_outputs=TRUE, dependencies = c("Depends", "Imports", "LinkingTo"), lib=Sys.getenv("R_LIBS_USER", unset=NA));
+install.packages(c(pkgvec), quiet = TRUE, keep_outputs=FALSE, dependencies = c("Depends", "Imports", "LinkingTo"), lib=Sys.getenv("R_LIBS_USER", unset=NA));
